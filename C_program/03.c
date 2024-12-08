93d9d810c1f4bef7d513b6e4e854053984fa7a1e
Jello <johnwangjl@gmail.com>
2024-12-08 15:36:55 +0800

    Initial commit

---
+++ C_program/03.c
@@ -0,0 +1,23 @@
+//#include <stdio.h>
+//
+//int main(void) {
+//    char name[50];
+//    int age;
+//    float grade;
+//
+//    printf("请输入你的姓名：");
+//    scanf("%s", name); // 读取字符串
+//
+//    printf("请输入你的年龄：");
+//    scanf("%d", &age); // 读取整数
+//
+//    printf("请输入你的成绩：");
+//    scanf("%f", &grade); // 读取浮点数
+//
+//    printf("\n你的信息如下：\n");
+//    printf("姓名：%s\n", name);
+//    printf("年龄：%d 岁\n", age);
+//    printf("成绩：%.2f 分\n", grade);
+//
+//    return 0;
+//}
---
+++ C_program/04.c
@@ -0,0 +1,66 @@
+/*
+ 1. 修改猜数字游戏：
+     - 限制玩家最多猜 10 次。
+     - 如果猜错 10 次，显示游戏失败。
+ */
+
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <time.h>
+//
+//int main(void) {
+//    int target, guess, num;
+//
+//    srand((unsigned)time(NULL));
+//    target = rand() % 100 + 1;
+//
+//    num = 0;
+//
+//    printf("欢迎来到猜数字游戏！\n");
+//    printf("系统已生成一个 1~100 的数字，请猜测：\n");
+//
+//    while (1) {
+//        if (num >= 10) {
+//            printf("你已经猜了 10 次，游戏失败！\n");
+//            break;
+//        }
+//        printf("请输入你的猜测：");
+//        scanf("%d", &guess);
+//
+//        if (guess < target) {
+//            printf("太小了，请再试一次！\n");
+//        } else if (guess > target) {
+//            printf("太大了，请再试一次！\n");
+//        } else {
+//            printf("恭喜你，猜对了！\n");
+//            break;
+//        }
+//        num++;
+//    }
+//
+//    return 0;
+//}
+
+/*
+ 2. 编写程序，判断一个输入的年份是否是闰年。
+ 
+    满足以下条件之一的年份为闰年：
+     • 能被4整除，但不能被100整除。
+     • 能被400整除。
+ */
+
+#include <stdio.h>
+
+int main(void) {
+    int year;
+    printf("Enter a year: ");
+    scanf("%d", &year);
+
+    if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+        printf("%d is a leap year.\n", year);
+    } else {
+        printf("%d is not a leap year.\n", year);
+    }
+    
+    return 0;
+}
